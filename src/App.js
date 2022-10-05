d4ebac05109b578c5e2b6f90cdb93d30f88946e0
boluwatifee1 <boluwatifee4@gmail.com>
2022-10-05 08:21:09 +0100

    implementing using tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { useState, useEffect } from 'react';
 import Modal from './components/modal';
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [showDivModal, setShowDivModal] = useState(false);
+  const targetEl = document.getElementById('defaultModal');
+  const targetDivEl =  document.getElementById('defaultModalDiv');
+  // open and hide modaal
+  const openModal = () => {
+    targetEl.classList.remove('hidden');
+  };
+
+  const openDivModal = () => {
+    targetDivEl.classList.remove('hidden');
+  };
+
+  const closeModal = () => {
+    targetEl.classList.add('hidden');
+  };
+
+  const closeDivModal = () => {
+    targetDivEl.classList.add('hidden');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,36 +30,67 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          style={{ cursor: "pointer" }}
-          onClick={() => setShowModal(true)}
-        >
-          Launch component Modal
-        </a>
-        <a  className="App-link"
-          style={{ cursor: "pointer" }} onClick={() => setShowDivModal(true)} >Launch Div Modal</a>
-      </header>
-      {
-        showModal ? <Modal className="fade-up" toggleModal={setShowModal} /> : null
-      }
-     
-      <div>
+        <button
+          onClick={openModal}
+          className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
+          Toggle Component modal
+        </button>
+        <button
+          onClick={openDivModal}
+          className="block my-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModalDiv">
+          Toggle Div modal
+        </button>
 
-        {showDivModal ? (
-          <div className="modal">
-            <div className="modal-content">
-              <div className="close">
-                <span className="close-btn" onClick={() => setShowDivModal(false)}>
-                  &times;
-                </span>
-              </div>
-              <h1>Modal</h1>
-              <p>Modal content</p>
+      </header>
+
+      {/* modal component */}
+      <Modal
+        className="fade-up"
+        id={"defaultModal"}
+        closeModalFunction={closeModal}
+      />
+
+      {/* Modal div */}
+
+      <div id="defaultModalDiv" tabIndex="-1" aria-hidden="true" className={`hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full`}>
+                <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                    {/* Modal content  */}
+                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        {/* Modal header */}
+                        <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                Terms of Service
+                            </h3>
+                            <button
+                                onClick={closeDivModal}
+                                type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModalDiv">
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                        {/*  Modal body  */}
+                        <div className="p-6 space-y-6">
+                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                            </p>
+                            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                            </p>
+                        </div>
+                        {/* <!-- Modal footer --> */}
+                        <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                            <button
+                                onClick={closeDivModal}
+                                data-modal-toggle="defaultModalDiv" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                            <button
+                                onClick={closeDivModal}
+                                data-modal-toggle="defaultModalDiv" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        ) : null}
-      </div>
+
+
     </div>
   );
 }
